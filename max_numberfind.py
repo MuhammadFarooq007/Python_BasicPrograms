31961b2bc1b1238a0038c24421ae34b8dd9d9d76
Muhammad Farooq <80383384+MuhammadFarooq007@users.noreply.github.com>
2022-06-08 17:42:09 +0500

    Add files via upload

---
+++ max_numberfind.py
@@ -0,0 +1,16 @@
+
+
+def maximum(a, b):
+    if a >= b:
+        print("Maximum Number is = ")
+        print(a)
+
+    else:
+        print("Maximum Number is = ")
+        print(b)
+
+
+number_1 = int(input("Enter First Number: "))
+number_2 = int(input("Enter Second Number: "))
+
+print(maximum(number_1, number_2))
